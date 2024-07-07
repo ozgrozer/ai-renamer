9d6dfb6fda20d66bd2a2af2e3079c2f658d09ce9
mac <ozgr@live.com>
2024-07-07 14:48:14 +0300

    Process path

---
+++ src/processPath.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const fs = require('fs').promises
+
+const processFile = require('./processFile')
+
+module.exports = async ({ inputPath }) => {
+  try {
+    const stats = await fs.stat(inputPath)
+
+    if (stats.isDirectory()) {
+      const files = await fs.readdir(inputPath)
+      for (const file of files) {
+        const filePath = path.join(inputPath, file)
+        const fileStats = await fs.stat(filePath)
+        if (fileStats.isFile()) {
+          await processFile({ filePath })
+        }
+      }
+    } else if (stats.isFile()) {
+      await processFile({ filePath: inputPath })
+    }
+  } catch (err) {
+    console.log('Error:', err.message)
+  }
+}
