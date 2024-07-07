6d00460878b19415a609cd47de16b93dcd168991
mac <ozgr@live.com>
2024-07-07 16:40:17 +0300

    Use that new name

--- src/processFile.js
+++ src/processFile.js
@@ -1,10 +1,11 @@
 const path = require('path')
 const fs = require('fs').promises
 
+const getNewName = require('./getNewName')
 const readFileContent = require('./readFileContent')
 const isProcessableFile = require('./isProcessableFile')
 
-module.exports = async ({ filePath }) => {
+module.exports = async ({ model, filePath }) => {
   try {
     if (!isProcessableFile({ filePath })) {
       console.log(`Skipping file: ${filePath}`)
@@ -17,13 +18,17 @@ module.exports = async ({ filePath }) => {
       return
     }
 
-    const newName = 'test'
+    const newName = await getNewName({ model, content })
+    if (!newName) {
+      console.log(`No new name: ${filePath}`)
+      return
+    }
 
     const dir = path.dirname(filePath)
     const ext = path.extname(filePath)
     const newPath = path.join(dir, newName + ext)
     await fs.rename(filePath, newPath)
-    console.log(`Renamed ${filePath} to ${newPath}`)
+    console.log(`Renamed: ${filePath} to ${newPath}`)
   } catch (err) {
     throw new Error(err.message)
   }
