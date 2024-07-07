d1f3cf935fac6f46da831155b25d54ca27a86567
mac <ozgr@live.com>
2024-07-07 14:47:14 +0300

    Add supported file extensions

---
+++ src/supportedExtensions.js
@@ -0,0 +1,28 @@
+module.exports = [
+  // general programming languages
+  '.js', '.jsx', '.ts', '.tsx', '.py', '.rb', '.php', '.java', '.c', '.cpp',
+  '.h', '.hpp', '.cs', '.go', '.rs', '.swift', '.kt', '.scala', '.groovy',
+  '.lua', '.pl', '.pm', '.r', '.dart', '.f', '.f90', '.f95', '.m', '.asm',
+  '.vb', '.coffee', '.elm', '.erl', '.ex', '.exs', '.hs', '.clj', '.cljs',
+
+  // web development
+  '.html', '.htm', '.css', '.scss', '.sass', '.less', '.vue', '.svelte',
+
+  // markup and data
+  '.md', '.markdown', '.json', '.xml', '.yaml', '.yml', '.csv', '.svg',
+
+  // shell and scripting
+  '.sh', '.bash', '.zsh', '.fish', '.ps1', '.bat', '.cmd', '.vbs',
+
+  // database and query languages
+  '.sql', '.graphql', '.gql',
+
+  // template languages
+  '.ejs', '.pug', '.jade', '.hbs', '.twig', '.liquid',
+
+  // other
+  '.txt', '.log', '.diff', '.patch', '.proto', '.tex',
+
+  // handled separately in code
+  '.pdf'
+]
