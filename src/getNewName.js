6e30d4f8ef3d489517d804150b5fd36cea6da6c3
mac <ozgr@live.com>
2024-07-07 20:04:24 +0300

    Update prompt

--- src/getNewName.js
+++ src/getNewName.js
@@ -1,8 +1,8 @@
 const ollama = require('ollama').default
 
-module.exports = async ({ model, _case, content }) => {
+module.exports = async ({ model, _case, chars, content }) => {
   try {
-    const prompt = `Given the following file content, suggest a concise and descriptive filename in ${_case} format. Don't include the extension. Don't include any other text or description. Just the filename.\n\n${content}`
+    const prompt = `Given the following file content, suggest a concise and descriptive filename in ${_case} format. Make the maximum number of characters ${chars} in the new filename. Don't include the extension. Don't include any other text or description. Just the filename.\n\n${content}`
     const res = await ollama.generate({ model, prompt })
     return res.response.trim()
   } catch (err) {
