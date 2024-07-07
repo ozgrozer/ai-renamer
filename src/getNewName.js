837ce1499deb546227862b620820001691cf66c7
mac <ozgr@live.com>
2024-07-07 16:40:10 +0300

    Get new name with ollama

---
+++ src/getNewName.js
@@ -0,0 +1,11 @@
+const ollama = require('ollama').default
+
+module.exports = async ({ model, content }) => {
+  try {
+    const prompt = `Given the following file content, suggest a concise and descriptive filename in kebab-case format. Don't include the extension. Don't include any other text or description. Just the filename.\n\n${content}`
+    const res = await ollama.generate({ model, prompt })
+    return res.response.trim()
+  } catch (err) {
+    throw new Error(err.message)
+  }
+}
