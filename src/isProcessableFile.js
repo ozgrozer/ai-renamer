dfadc74b3f64c27db3de5cebf50e2b128ad13440
mac <ozgr@live.com>
2024-07-07 19:23:16 +0300

    Skip .DS_Store files

--- src/isProcessableFile.js
+++ src/isProcessableFile.js
@@ -3,6 +3,9 @@ const path = require('path')
 const supportedExtensions = require('./supportedExtensions')
 
 module.exports = ({ filePath }) => {
+  const fileName = path.basename(filePath)
+  if (fileName === '.DS_Store') return false
+
   const ext = path.extname(filePath).toLowerCase()
   return supportedExtensions.includes(ext)
 }
