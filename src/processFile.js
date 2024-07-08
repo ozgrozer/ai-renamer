1e163a9a6603d44ebe42952536e28b21bbb7ee2d
mac <ozgr@live.com>
2024-07-08 15:45:02 +0300

    If file is an image pass the path to getNewName function

--- src/processFile.js
+++ src/processFile.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const fs = require('fs').promises
 
+const isImage = require('./isImage')
 const getNewName = require('./getNewName')
 const readFileContent = require('./readFileContent')
 const isProcessableFile = require('./isProcessableFile')
@@ -8,6 +9,7 @@ const isProcessableFile = require('./isProcessableFile')
 module.exports = async ({ model, _case, chars, language, filePath }) => {
   try {
     const fileName = path.basename(filePath)
+    const ext = path.extname(filePath).toLowerCase()
 
     if (fileName === '.DS_Store') return
 
@@ -16,20 +18,25 @@ module.exports = async ({ model, _case, chars, language, filePath }) => {
       return
     }
 
-    const content = await readFileContent({ filePath })
-    if (!content) {
-      console.log(`🔴 No text content: ${fileName}`)
-      return
+    let content
+    const images = []
+    if (isImage({ ext })) {
+      images.push(filePath)
+    } else {
+      content = await readFileContent({ filePath })
+      if (!content) {
+        console.log(`🔴 No text content: ${fileName}`)
+        return
+      }
     }
 
-    const newName = await getNewName({ model, _case, chars, content, language })
+    const newName = await getNewName({ model, _case, chars, content, language, images })
     if (!newName) {
       console.log(`🔴 No new name: ${fileName}`)
       return
     }
 
     const dir = path.dirname(filePath)
-    const ext = path.extname(filePath)
     const newFileName = newName + ext
     const newPath = path.join(dir, newFileName)
     await fs.rename(filePath, newPath)
