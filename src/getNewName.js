eb39e1439803a18e7ef7ded422fb991aa6e8a5b1
mac <ozgr@live.com>
2024-07-08 15:33:46 +0300

    Only show content if content is not empty

--- src/getNewName.js
+++ src/getNewName.js
@@ -1,8 +1,8 @@
 const ollama = require('ollama').default
 
-module.exports = async ({ model, _case, chars, content, language }) => {
+module.exports = async ({ model, _case, chars, content, language, images }) => {
   try {
-    const prompt = `
+    let prompt = `
 Generate a concise, descriptive filename for the following content:
 - Use ${_case} format
 - Maximum ${chars} characters
@@ -11,11 +11,15 @@ Generate a concise, descriptive filename for the following content:
 - Avoid special characters
 - Output only the filename
 
-IMPORTANT: Your entire response should be just the filename in ${_case} format, in ${language} language, and max ${chars} characters. Do not include any other text.
+IMPORTANT: Your entire response should be just the filename in ${_case} format, in ${language} language, and max ${chars} characters. Do not include any other text.`
 
+    if (content) {
+      prompt += `
 Content:
 ${content}`
-    const res = await ollama.generate({ model, prompt })
+    }
+
+    const res = await ollama.generate({ model, prompt, images })
     return res.response.trim()
   } catch (err) {
     throw new Error(err.message)
