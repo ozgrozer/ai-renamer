ebeb9ec163e16a3798a19b0874ee206562bc6d4d
M <91066601+mishalhossin@users.noreply.github.com>
2024-08-15 23:28:43 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 #!/usr/bin/env node
 
-const processPath = require('./processPath')
-const configureYargs = require('./configureYargs')
+const processPath = require('./processPath');
+const configureYargs = require('./configureYargs');
 
 const main = async () => {
   try {
-    const { argv, config } = await configureYargs()
-    const [inputPath] = argv._
+    const { argv, config } = await configureYargs();
+    const inputPath = argv._[0];
+    const defaultFileRegex = argv['file-regex'];
 
     if (!inputPath) {
-      console.log('🔴 Please provide a file or folder path')
-      process.exit(1)
+      console.log('🔴 Please provide a file or folder path');
+      process.exit(1);
     }
 
-    await processPath({ ...config, inputPath })
+    await processPath({ 
+      ...config, 
+      inputPath, 
+      defaultFileRegex,
+    });
   } catch (err) {
-    console.log(err.message)
+    console.log(err.message);
   }
-}
+};
 
-main()
+main();
